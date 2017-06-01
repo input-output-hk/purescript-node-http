447d84078c0ad322859de3a17279bde78aff768a
Ante Kegalj <akegalj@gmail.com>
2017-06-01 15:41:52 +0200

    test

--- src/Node/HTTP/Client.js
+++ src/Node/HTTP/Client.js
@@ -4,11 +4,17 @@ var http = require("http");
 var https = require("https");
 
 exports.requestImpl = function (opts) {
+    console.log('step1')
   return function (k) {
+    console.log('step2')
     return function () {
+    console.log('step3')
       var lib = opts.protocol === "https:" ? https : http;
+    console.log('step4')
       return lib.request(opts, function (res) {
+    console.log('step5')
         k(res)();
+    console.log('step6')
       });
     };
   };
